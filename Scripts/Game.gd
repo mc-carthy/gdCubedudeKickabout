2364dc4805a20f4357247efdd158c81b899b165d
Mick McCarthy <mick@mccarthy.tech>
2020-02-24 20:52:48 +0000

    Freeze ball when goal scored

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,8 +1,14 @@
 extends Spatial
 
+onready var Ball = get_tree().get_root().find_node("Ball", true, false)
+
 func _ready():
 	pass
 
 
 func _on_GoalDetection_body_entered(body, player_scored_id):
+	Ball.axis_lock_linear_x = true
+	Ball.axis_lock_linear_y = true
+	Ball.axis_lock_linear_z = true
 	print("Player " + str(player_scored_id) + " has scored")
+	
