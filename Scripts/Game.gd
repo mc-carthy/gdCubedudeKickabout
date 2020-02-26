5bbd19edf72a890ba6531acf615c1feb6ded6dbb
Mick McCarthy <mick@mccarthy.tech>
2020-02-26 20:35:45 +0000

    Wire up score GUI

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,6 +2,10 @@ extends Spatial
 
 onready var Ball = get_tree().get_root().find_node("Ball", true, false)
 
+var Player1_score = 0
+var Player2_score = 0
+export var max_score = 5
+
 func _ready():
 	reset_pitch()
 
@@ -12,7 +16,7 @@ func _on_GoalDetection_body_entered(body, player_scored_id):
 	Ball.axis_lock_linear_z = true
 	get_tree().call_group("player", "can_move", false)
 	$Timer.start()
-	print("Player " + str(player_scored_id) + " has scored")
+	update_score(player_scored_id)
 	
 
 func _on_Timer_timeout():
@@ -29,3 +33,17 @@ func reset_pitch():
 	Ball.translation = ball_spawn.translation
 
 	get_tree().call_group("player", "reset")
+
+func update_score(player_id):
+	var new_score
+	if player_id == 1:
+		Player1_score += 1
+		new_score = Player1_score
+	else:
+		Player2_score += 1
+		new_score = Player2_score
+
+	$GUI.update_score(new_score, player_id)
+	check_game_over(new_score)
+
+func check_game_over(score):
